dfee7589866fe9773ec95d78a0911bad7ed9ba6a
ovozzx <takiwow19@gmail.com>
2026-08-26 18:58:44 +0900

    fix: /seat 경로에도 로그인 체크 Interceptor 적용

--- src/main/java/com/cafe/app/common/config/WebConfig.java
+++ src/main/java/com/cafe/app/common/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor)
-                .addPathPatterns("/seat/**");
+                .addPathPatterns("/seat", "/seat/**");
     }
 
     /*
